266d59d62cfa85e918bc06f2c1f277d047ba1469
NairaAlmelegy <nairaalmelegy@gmail.com>
2024-05-24 18:50:50 +0300

    new updates

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Naira', lastName: 'Almelegy'};
   return (
-    <main>
-        SIDEBAR
-            {children}
-    </main>
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn}/>
+      {children}
+        </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,27 @@
-import HeaderBox from '@/components/HeaderBox'
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 
 const Home = () => {
+    const loggedIn = { firstName: 'Naira'};
     return (
         <section className = "home">
             <div className = "home-content">
                 <header className='home-header'>
-                    <HeaderBox/>
+                    <HeaderBox 
+                      type="greeting"
+                      title="Welcome"
+                      user={loggedIn?.firstName || 'Guest'}
+                      subtext="Access and manage your account and transactions efficiently."                
+                    />
+
+                    <TotalBalanceBox 
+                      accounts={[]}
+                      totalBanks={1}
+                      totalCurrentBalance={1250.35}
+                    />
                 </header>
+
+                RECENT TRANSACTIONS
             </div>
         </section>
     )
