453a4ec2775a4adf70e3effc0e2319d98b5e3333
NairaAlmelegy <nairaalmelegy@gmail.com>
2024-05-31 16:06:40 +0300

    push updATES

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'now-blue': ['semibold', 'sans-serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
